db8d19092d1ba7b89936ebffc5289e788d7795b0
marcosgfrites <49597978+marcosgfrites@users.noreply.github.com>
2020-05-18 10:01:55 -0300

    Clientes
    Se comenzó a trabajar con Clientes. Se crea la clase de ClientesQuerys.cs + ClientesController + La View de Consultas (obtiene el listado de clientes).

---
+++ AccesoDatos/ClientesQuerys.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AccesoDatos
+{
+    public class ClientesQuerys
+    {
+        public List<Clientes> GetClientes()
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConexionPracticoMVC"].ConnectionString);
+            List<Clientes> listado = new List<Clientes>();
+            listado = con.Query<Clientes>("SELECT Codigo,RazonSocial,FechaCreacion,IdUsuario FROM Clientes ORDER BY RazonSocial ASC").ToList();
+
+            return listado;
+        }
+
+    }
+}
