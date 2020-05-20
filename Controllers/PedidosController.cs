03d3b65a1a995e40a189885ccc21dd7e952de4b7
marcosgfrites <49597978+marcosgfrites@users.noreply.github.com>
2020-05-20 10:44:08 -0300

    Inicio de Pedidos
    Se busca primero, que la view permita una selección del cliente.
    Mediante un input permitir ingresar una palabra o caracter; y así obtener un listado de todos los clientes que contengan dicho nombre o caracter en su razon social.

--- Controllers/PedidosController.cs
+++ Controllers/PedidosController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AccesoDatos;
+using Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +13,21 @@ namespace PracticoMVC.Controllers
         // GET: Pedidos
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult SelectCliente()
+        {
+            ClientesQuerys cq = new ClientesQuerys();
+            List<Clientes> clientes = new List<Clientes>();
+            clientes = cq.GetClientes();
+            UsuariosQuerys uq = new UsuariosQuerys();
+            List<Usuarios> usuarios = new List<Usuarios>();
+            usuarios = uq.GetUsuarios();
+
+            ViewBag.ListaClientes = clientes;
+            ViewBag.ListaUsuarios = usuarios;
+
             return View();
         }
     }
